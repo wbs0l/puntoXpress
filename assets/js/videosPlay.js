4f25424dbe7112692af62503a373236aa71d59d9
Daninson <108442915+wbs0l@users.noreply.github.com>
2025-06-12 21:44:29 -0400

    Add files via upload

---
+++ assets/js/videosPlay.js
@@ -0,0 +1,10 @@
+function playVideo2(){
+    let videoFINAL= document.getElementById("videoFinal2")
+    videoFINAL.play();
+    videoFINAL.loop="true";
+}
+function playVideo1(){
+    let videoF1=document.getElementById("videoFinal1")
+    videoF1.play();
+    videoF1.loop="true";
+}
